40c26b357b23eeb1e4ba408e3cc36cde1d8b0daf
rivka123 <gasasa255@gmail.com>
2022-03-10 23:12:21 +0200

    for exs

--- for/for ex .py
+++ for/for ex .py
@@ -30,6 +30,42 @@ for i in range(10,20):
         print(i)
         i+=1
 
+# 5
+sum = 0
+for i in range(1,14):
+   sum=sum+i
+   print(sum)
+
+# 6
+for i in range(1,11):
+    for j in range(1,11):
+        print(i*j,end=' ')
+    print()
+
+# 7
+for i in range(100,5000,4):
+    print(i)
+
+# 8
+for i in range(5,2500):
+    if i %6==0:
+        print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
