3eb944171683aeb34e2a3ef6a1f7fa0ec745ff78
rivka123 <gasasa255@gmail.com>
2022-03-10 15:33:01 +0200

    for exs

--- for/for ex .py
+++ for/for ex .py
@@ -3,7 +3,7 @@ for i in range(1,20):
     if i%2!=0:
         print(i)
 
-# 2
+2
 num = int(input("please enter number:"))
 ch = int(input("please enter number 1 to odd number \nor number 2 to even number :"))
 if ch == 1:
@@ -14,6 +14,23 @@ elif ch == 2:
         print(i)
 else:
     print("Insert numbers 1 or 2")
+
+3
+i = 100
+for i in range(100,500):
+    if i == 287:
+        print(i)
+        break
+
+4
+i=0
+for i in range(10,20):
+    flag=0
+    if i != 13:
+        print(i)
+        i+=1
+
+
 
 
 
